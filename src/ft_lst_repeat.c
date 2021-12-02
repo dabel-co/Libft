dc054830a9e059d2f7930007d9d9d67ff6805e46
dabel-co <dabel-co@c1r13s5.42madrid.com>
2021-12-02 13:06:37 +0100

    fixed ft_lst_repeat to work only with numbers for the push_swap

--- src/ft_lst_repeat.c
+++ src/ft_lst_repeat.c
@@ -6,7 +6,7 @@
 /*   By: dabel-co <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 19:33:37 by dabel-co          #+#    #+#             */
-/*   Updated: 2021/11/16 15:18:29 by dabel-co         ###   ########.fr       */
+/*   Updated: 2021/12/02 13:04:22 by dabel-co         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_lst_repeat(t_list *a)
 	last = ft_lstlast(a);
 	while (aux->next != NULL)
 	{
-		if (aux->content == last->content)
+		if (*(int *)aux->content == *(int *)last->content)
 			return (-1);
 		aux = aux->next;
 	}
