f4897a46d8f71d50b8ab81715dc16835e0758903
“dabel-co” <dabel-co@student.42madrid.com>
2022-03-07 14:48:31 +0100

    added ft_strtrimfree

--- src/ft_strtrim_free.c
+++ src/ft_strtrim_free.c
@@ -3,19 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim_free.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dabel-co <dabel-co@student.42madrid>       +#+  +:+       +#+        */
+/*   By: vguttenb <vguttenb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 14:29:37 by dabel-co          #+#    #+#             */
-/*   Updated: 2022/03/07 14:29:39 by dabel-co         ###   ########.fr       */
+/*   Updated: 2022/03/07 14:48:17 by dabel-co         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim_free(char *s1, char const *set)
 {
 	int		x;
 	char	*aux;
+	char	*ret;
 	
 	if (!s1 || !set)
 		return (NULL);
@@ -25,8 +26,10 @@ char	*ft_strtrim(char const *s1, char const *set)
 	x = ft_strlen(s1);
 	while (x && ft_strchr(set, s1[x]))
 		x--;
-	free(aux);
 	if (!x)
-		return (ft_strdup(""));
-	return (ft_substr(s1, 0, x + 1));
+		ret = ft_strdup("");
+	else
+		ret = ft_substr(s1, 0, x + 1);
+	free(aux);
+	return (ret);
 }
--- src/libft.h
+++ src/libft.h
@@ -6,7 +6,7 @@
 /*   By: dabel-co <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 18:28:48 by dabel-co          #+#    #+#             */
-/*   Updated: 2022/03/07 14:29:54 by dabel-co         ###   ########.fr       */
+/*   Updated: 2022/03/07 14:47:17 by dabel-co         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strjoin_free(char *keeper, char *buffer);
 char	*ft_strtrim(char const *s1, char const *set);
-char	*ft_strtrim_free(char const *s1, char const *set);
+char	*ft_strtrim_free(char *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
