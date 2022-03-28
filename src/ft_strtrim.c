11a67f590944832f1b1d43afa30fb292c2b81e9b
“dabel-co” <dabel-co@student.42madrid.com>
2022-03-28 16:36:57 +0200

    fixed strtrim

--- src/ft_strtrim.c
+++ src/ft_strtrim.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dabel-co <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: vguttenb <vguttenb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/12 19:13:57 by dabel-co          #+#    #+#             */
-/*   Updated: 2022/03/07 14:47:58 by dabel-co         ###   ########.fr       */
+/*   Updated: 2022/03/23 16:35:59 by vguttenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,10 @@ char	*ft_strtrim(char const *s1, char const *set)
 		return (NULL);
 	while (*s1 && ft_strchr(set, *s1))
 		s1++;
+	if (!*s1)
+		return (ft_strdup(""));
 	x = ft_strlen(s1);
 	while (x && ft_strchr(set, s1[x]))
 		x--;
-	if (!x)
-		return (ft_strdup(""));
 	return (ft_substr(s1, 0, x + 1));
 }
--- src/ft_strtrim_free.c
+++ src/ft_strtrim_free.c
@@ -6,7 +6,7 @@
 /*   By: vguttenb <vguttenb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 14:29:37 by dabel-co          #+#    #+#             */
-/*   Updated: 2022/03/07 14:48:17 by dabel-co         ###   ########.fr       */
+/*   Updated: 2022/03/23 16:36:52 by vguttenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,15 @@ char	*ft_strtrim_free(char *s1, char const *set)
 	aux = s1;
 	while (*s1 && ft_strchr(set, *s1))
 		s1++;
-	x = ft_strlen(s1);
-	while (x && ft_strchr(set, s1[x]))
-		x--;
-	if (!x)
+	if (!*s1)
 		ret = ft_strdup("");
-	else
+	else 
+	{
+		x = ft_strlen(s1) - 1;
+		while (x && ft_strchr(set, s1[x]))
+			x--;
 		ret = ft_substr(s1, 0, x + 1);
+	}
 	free(aux);
 	return (ret);
 }
